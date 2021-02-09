ba41251959e728b289a5f6f27cc009fb6f439e98
usman <anwerusman63@gmail.com>
2021-02-08 17:05:34 -0800

    Initial commit

---
+++ app/src/main/java/com/example/eventhandling/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.eventhandling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b= findViewById(R.id.btn);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+                
+            }
+        });
+    }
+}
